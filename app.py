6e70abacd423f55fa8ec0f4d39d8d3ca68a47bd7
Tugdual <tugdual.vanthuyne@ipsa.fr>
2025-11-24 15:20:44 +0100

    fichier ajoute (fonction info en plus)

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import sys
 
 
 from PySide6 import QtUiTools
+from PySide6.QtGui import QPainter, QColor
 from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QPushButton, QListView, QFrame
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile, QIODevice, Slot
-from avion import classavion
-
+from matplotlib.backend_bases import button_press_handler, MouseEvent
+import random as rnd
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -49,14 +50,68 @@ class MainWindow(QMainWindow):
     def demo(self):
         """Slot appelé lorsque le bouton est cliqué"""
         QMessageBox.information(self, "Message", "Hello! Le bouton a été cliqué!")
-        print("Hello from slot!hhhh")
+        print("Hello from slot!")
 
-    @Slot()
-    def affichage(self):
-        rect = self.frame.geometry()
-        self.height = rect.height()
-        self.width = rect.width()
-        print(self.height,self.width)
+
+class Radar(QFrame):
+    def __init__(self, parent = None, espace_aerien = None):
+        super().__init__(parent)
+        self.espace_aerien = espace_aerien
+        self.setMouseTracking(True)
+        self.setMinimumSize(400, 400)
+    def paintEvent(self, event):
+        for avion in self.espace_aerien:
+            painter = QPainter(self)
+            rect = self.rect()
+            painter.fillRect(rect, QColor(255, 255, 255))
+            try:
+
+                SIM_WIDTH_KM = 2 * RAYON_ESPACE_AERIEN_KM
+            except NameError:
+                SIM_WIDTH_KM = 100
+
+            echelle_px_par_km = min(rect.width(), rect.height()) / SIM_WIDTH_KM
+            centre_x, centre_y = rect.width() / 2, rect.height() / 2
+
+
+            for avion in self.espace_aerien.liste_avions:
+
+
+                x_px = avion.x * echelle_px_par_km + centre_x
+                y_px = -avion.y * echelle_px_par_km + centre_y
+
+
+                color = QColor(0, 255, 0)
+                if avion.est_en_urgence:
+                    color = QColor(255, 0, 0)
+                elif avion.est_selectionne:
+                    color = QColor(255, 255, 0)
+
+                painter.setPen(QPen(color, 2))
+                painter.setBrush(QBrush(color))
+
+
+                painter.drawEllipse(x_px - 4, y_px - 4, 8, 8)
+
+
+                painter.setPen(QPen(QColor(255, 255, 255)))
+                painter.drawText(x_px + 5, y_px + 5, avion.id_vol)
+
+            painter.end()
+
+
+class EspaceAerien:
+    def __init__(self, rayon_km):
+        self.list_avion = []
+
+    def avionenplus(self,avion):
+        self.list_avion.append(avion)
+"""
+    def collision(self):
+        for i in self.list_avion:
+"""
+
+
 
 
 
--- avion.py
+++ avion.py
@@ -1,14 +1,30 @@
+import random as rnd
+from math import*
+from PySide6.QtWidgets import QFrame
 
-class classavion():
-    def __init__(self, altitude, vitesse,cap, carburant):
-        self.altitutde = altitude
+
+
+class Avion():
+    def __init__(self, position_x, position_y, altitude, vitesse, cap, carburant):
+        self.x = position_x
+        self.y = position_y
+        self.altitude = altitude
         self.vitesse = vitesse
         self.cap = cap
-        self.carburant=carburant
+        self.carburant = carburant
+    def new_coords(self):
+        self.x = rnd.randint(self.cap[0], self.cap[1])
+        self.y = rnd.randint(self.cap[0], self.cap[1])
+        print(f"x : {self.x}")
+
+
+avion = Avion(30,30,2000,200,[0,360],1000)
+avion.new_coords()
 
 
 
 
 
-a1 = classavion(30000, 500, 90)
-a2 = classavion(20000, 400, 20)
+
+
+
